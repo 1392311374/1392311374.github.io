8948a1396e599d8eb9f8e144e179ec2310895ca5
WANGXV <1392311374@qq.com>
2021-04-14 18:36:10 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function jump (){
+  window.location.href="https://www.bilibili.com/video/BV13x411P7eK"
+}
+function center (){
+  window.location.href="https://www.bilibili.com/video/BV16x411579p"
+}
+var dom=document.getElementsByClassName("tex")[0]
+dom.style.textAlign="center"
